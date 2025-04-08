94f309a66d6d465500a47bdea00c478effc79357
Nicolas <nicolasjn22@gmail.com>
2025-04-08 20:02:41 -0300

    feat: animations, buttons and navbar

--- src/pages/Home/sections/Hero/hero.tsx
+++ src/pages/Home/sections/Hero/hero.tsx
@@ -1,15 +1,21 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import theme from "../../../../theme";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
-    const StyledHero = styled("div")(()=>({
-        backgroundColor: "black",
-        height: "100vh"
+    const StyledHero = styled("div")(({theme})=>({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems:"center"
     }))
     const StyledImg = styled("img")(()=>({
-        width: "100%",
+        width: "80%",
+        border: `1px solid ${theme.palette.primary.contrastText}`,
         borderRadius: "50%"
     }))
 
@@ -18,25 +24,34 @@ const Hero = () => {
             <StyledHero>
                 <Container maxWidth="lg">
                     <Grid container spacing={2}>
-                        <Grid size={{xs: 12, md: 4}}>
-                            <StyledImg src={Avatar}/>
+                        <Grid size={{xs: 12, md: 5}}>
+                            <Box position="relative">
+                                <Box position="absolute" width="150%" top={-100} right={0}>
+                                    <AnimatedBackground></AnimatedBackground>
+                                </Box>
+                                <Box position={"relative"} textAlign="center" alignItems="center">
+                                    <StyledImg src={Avatar}/>
+                                </Box>
+                            </Box>
                         </Grid>
-                        <Grid size={{xs:12, md:8}}>
-                            <Typography color="primary" variant="h1" textAlign="center">Jorge Nicolas</Typography>
-                            <Typography color="primary" variant="h2" textAlign="center">I'm a software developer</Typography>
+                        <Grid size={{xs:12, md:7}}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Jorge Nicolas</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a software developer</Typography>
                             
-                            <Grid container  display="flex" justifyContent="center">
+                            <Grid container  display="flex" justifyContent="center" spacing={3} pt={5}>
                                 <Grid size={{xs: 12, md: 4}} display="flex" justifyContent="center">
-                                    <Button>
+                                    <StyledButton>
                                         <DownloadIcon/>
-                                        Download CV
-                                    </Button>
+                                        <Typography>Download CV</Typography>
+                                    </StyledButton>
                                 </Grid>
                                 <Grid size={{xs: 12, md: 4}} display="flex" justifyContent="center">
-                                        <Button>
+                                        <StyledButton>
                                             <EmailIcon/>
-                                            Contact Me
-                                        </Button>
+                                            <Typography>Contact Me</Typography>
+                                        </StyledButton>
+
+                                        {/* tempo do video 47:09*/}
                                 </Grid>
                             </Grid>
                         </Grid>
